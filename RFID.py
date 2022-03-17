6eca62d7ad7b56024d47d9fd797e5eae73ff44cc
Sid-Ravi <99811212+Sid-Ravi@users.noreply.github.com>
2022-03-17 09:04:58 -0400

    Update RFID.py

--- RFID.py
+++ RFID.py
@@ -3,21 +3,41 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle, Color
-from kivy.properties import ObjectProperty,NumericProperty,ListProperty
-from kivy.uix.spinner import Spinner
+from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import Screen
+from kivy.config import Config
+import Volume_Weight
 
-kv=Builder.load_file("rfidgraphics.kv")
+x = 800
+y = 600
+Config.set('graphics', 'resizable', False)
+Config.set('graphics', 'width', x)
+Config.set('graphics', 'height', y)
+Config.write()
 
-class RFIDGraphics(FloatLayout):
-	pass
 
 
 
-class Screen(App):
-	def build(self):
-		return RFIDGraphics()
 
-if __name__ == "__main__":
-    Screen().run()
+class RFIDGraphics(Screen):
+    kv = Builder.load_file("rfidgraphics.kv")
+    output=ObjectProperty(None)
+    screenbutton = ObjectProperty(None)
+
+    def on_enter(self, *args):
+        self.ids.screenbutton.background_color = (1, 0, 1, 1)
+        Number,Volume,Weight,Density=Volume_Weight.VWGraphics().data()
+        if(Volume== None or Weight==None or Density==None):
+            self.ids.output.text = "Redo Weight calculation"
+        elif(Number>26):
+            self.ids.output.text = "Pallets scanned exceed 26"
+        else:
+            self.ids.output.text=("Pallet: "+ str(Number)+"\nVolume: " + str(Volume)+"\nDensity: " + str(Density)+"\nWeight: " + str(Weight))
+
+    def SubmitRFID(self):
+        Number, Volume, Weight, Density = Volume_Weight.VWGraphics().data()
+        if (Volume != None and Weight != None and Density != None):
+            RFIDfile = open("RFIDOutput.txt", mode="w")
+            RFIDfile.write("Pallet: "+ str(Number)+"\nVolume: " + str(Volume)+"\nDensity: " + str(Density)+"\nWeight: " + str(Weight))
+            RFIDfile.close
